f417fa9ee2fbb49704b7a9268423d481532059b7
benapier <napier.ben.e@gmail.com>
2019-11-23 23:22:50 +0000

    Delete old stuff

--- visualisation/main.py
+++ visualisation/main.py
@@ -213,33 +213,3 @@ vis = Visualisation(width, network)
 
 
 main()
-
-# Pick an edge
-# edge = edge_list[random.randint(0, len(edge_list) - 1)]
-# print(edge)
-#
-# # Lets animate a packet
-# # Initial position
-# packet = Circle(nodes[node_list.index(edge[0])].getCenter(), 5)
-# packet.setFill("black")
-# packet.draw(win)
-# # Calculate difference vector
-# frame_count = 25
-# dx = (nodes[node_list.index(edge[1])].getCenter().x - nodes[node_list.index(edge[0])].getCenter().x) * math.pow(25, -1)
-# dy = (nodes[node_list.index(edge[1])].getCenter().y - nodes[node_list.index(edge[0])].getCenter().y) * math.pow(25, -1)
-#
-# # while True:
-# #     win.getMouse()
-# #     packet.move(dx / 10, dy / 10)
-#
-#
-# start_time = time.time()
-# frame_rate = 60
-# i = 0
-# win.getMouse()
-# while True:
-#     time.sleep(math.pow(frame_rate, -1) - ((time.time() - start_time) % math.pow(frame_rate, -1)))
-#     win.checkMouse()
-#     if i < frame_count:
-#         packet.move(dx, dy)
-#         i += 1
