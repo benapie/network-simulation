445e7d620934e1dd1ebf2cf0702aed52e675f8d4
jwhowes <jacob.w.howes@durham.ac.uk>
2019-11-23 15:36:44 +0000

    Implemented simple distance vector routing

--- src/basic_network/router.py
+++ src/basic_network/router.py
@@ -8,6 +8,29 @@ class Router:
         self.edges = []
         self.connected_to = []
         self.address = address
+        self.distance_vector = {}
+        for address in self.connected_to:
+            for edge in self.edges:
+                if edge.a.address == address:
+                    self.distance_vector[address] = edge.a.ticks_per_packet
+                    break
+                elif edge.b.address == address:
+                    self.distance_vector[address] = edge.b.ticks_per_packet
+                    break
+
+    def send_distance_vector(self):
+        for edge in self.edges:
+            if edge.a == self:
+                self.send_packet(edge.b, Packet({"HEAD": "DV", "CONTENT": self.distance_vector}))
+            else:
+                self.send_packet(edge.a, Packet({"HEAD": "DV", "CONTENT": self.distance_vector}))
+
+    def update_distance_vector(self, received_vector: Packet):
+        for node in received_vector["CONTENT"]:
+            if node not in self.distance_vector:
+                self.distance_vector[node] = received_vector["CONTENT"][node]
+            else:
+                self.distance_vecotr[node] = min([self.distance_vector[node], received_vector["CONTENT"][node]])
 
     def link_to(self, node: Router, ticks_per_packet: int) -> Edge:
         """Links this router to another router,
