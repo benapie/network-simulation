53ff55d23ad09d9f028975fac6cf745800b19998
benapier <napier.ben.e@gmail.com>
2019-11-24 00:13:28 +0000

    Made outline opacity sendable on send_packet

--- visualisation/main.py
+++ visualisation/main.py
@@ -189,26 +189,26 @@ class Packet:
         self.frame_count += 1
         return self.frame_count == self.frame_count_max
 
-
-def main():
-    start_time = time.time()
-    frame_rate = 50
-    frame_count = 0
-    while True:
-        frame_count += 1
-        time.sleep(math.pow(frame_rate, -1) - ((time.time() - start_time) % math.pow(frame_rate, -1)))
-        vis.tick()
-        vis.window.checkMouse()
-        edge = vis.network.edge_list[random.randint(0, len(vis.network.edge_list)) - 1]
-
-        vis.send_packet(edge.node_a_label, edge.node_b_label, random.randint(40, 100))
-
-
-## Testing!!
-width = 500
-random_network = randomly_generate_network()
-node_list, edge_list = circle_arrangement(width / 2 - 20, width / 2, width / 2, random_network[0], random_network[1])
-network = Network(node_list, edge_list)
-vis = Visualisation(width, network)
-
-main()
+# Following is testing stuff
+# def main():
+#     start_time = time.time()
+#     frame_rate = 50
+#     frame_count = 0
+#     while True:
+#         frame_count += 1
+#         time.sleep(math.pow(frame_rate, -1) - ((time.time() - start_time) % math.pow(frame_rate, -1)))
+#         vis.tick()
+#         vis.window.checkMouse()
+#         edge = vis.network.edge_list[random.randint(0, len(vis.network.edge_list)) - 1]
+#
+#         vis.send_packet(edge.node_a_label, edge.node_b_label, random.randint(40, 100))
+#
+#
+# ## Testing!!
+# width = 500
+# random_network = randomly_generate_network()
+# node_list, edge_list = circle_arrangement(width / 2 - 20, width / 2, width / 2, random_network[0], random_network[1])
+# network = Network(node_list, edge_list)
+# vis = Visualisation(width, network)
+#
+# main()
